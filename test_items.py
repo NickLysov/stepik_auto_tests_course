50140983c08936ec9b1199c706fbc18744da4ec5
Nikita <smartkillerrr@gmail.com>
2023-01-24 22:25:01 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -3,7 +3,7 @@ import time
 
 link="http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_items(browser):
+def test_items_find_button(browser):
     browser.get(link)
     #time.sleep(5)
     y = len(browser.find_elements(By.CLASS_NAME, "btn.btn-lg.btn-primary.btn-add-to-basket"))
