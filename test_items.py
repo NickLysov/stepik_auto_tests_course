f913129d6eb667ff67c52470127f71ac756d8f23
Nikita Lysov <smartkillerrr@gmail.com>
2023-01-24 21:51:21 +0300

    added test_items and conftest

---
+++ test_items.py
@@ -0,0 +1,10 @@
+from selenium.webdriver.common.by import By
+import time
+
+link="http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_items(browser):
+    browser.get(link)
+    time.sleep(5)
+    y = len(browser.find_elements(By.CLASS_NAME, "btn.btn-lg.btn-primary.btn-add-to-basket"))
+    assert y > 0, 'Требуемый селектор не был найден.'   
